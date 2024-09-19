fa2d90c16829e599f0fe9d0afb620df69bd075b5
PhoneSimDev <phonesimdev@gmail.com>
2024-09-19 01:54:39 -0300

    Show game name in title and when exporting save

--- system/config.js
+++ system/config.js
@@ -1,5 +1,5 @@
 const gameConfig = {
     devMode: true,
-    gameName: "PhoneSim",
-    gameKey: "123456" //Never change this value or the imported saves won't work.
+    name: "PhoneSim",
+    key: "123456" //Never change this value or the imported saves won't work.
 }
