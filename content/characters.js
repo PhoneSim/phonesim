7f2bad358c631c8a2c0948fdfeb5566f045bde11
PhoneSimDev <phonesimdev@gmail.com>
2024-09-25 15:10:09 -0300

    Facilitating the editing of characters and story-EDITOR

---
+++ content/characters.js
@@ -0,0 +1,50 @@
+/*
+Instructions:
+1. Write the story within the backticks and never outside of them.
+2. Do not delete or modify anything outside of the backticks.
+3. Copy and paste the fields below and modify them accordingly.
+
+Character: marcus
+Name: Marcus
+Perfil: b.jpg
+Available: yes
+Is Group: no
+Text Color: red
+Relationships: bro, sis
+
+Note: If the character doesn't need relationships you can put “no” as in the developer's example.
+*/
+
+
+const characters = `
+
+
+
+Character: dev
+Name: Developer
+Perfil: a.jpg
+Available: yes
+Is Group: no
+Text Color: #FFF
+Relationships: no
+
+Character: mc
+Name: MC
+Perfil: a.jpg
+Available: no
+Is Group: no
+Text Color: #FFF
+Relationships: no
+
+Character: anime
+Name: Anime Girl
+Perfil: b.jpg
+Available: no
+Is Group: no
+Text Color: pink
+Relationships: bro, sis
+
+
+
+
+`
---
+++ content/story.js
@@ -0,0 +1,9 @@
+/*
+Instructions:
+1. Write the story within the backticks and never outside of them.
+2. Do not delete or modify anything outside of the backticks.
+*/
+
+const story = `
+Creating my first game with PhoneSim
+`
